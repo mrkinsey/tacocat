499accb5d664676e3806ecee63f2a19ad35650a8
mrkinsey <mrkinsey10@gmail.com>
2023-08-23 21:01:59 -0400

    tacocat completed v1

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-// Call Hello World
-function helloWorld(){
-    alert("Hello World!");
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let returnObj = palindromeCheck(userString);
+    displayTacoCat(returnObj);
+}
+
+function palindromeCheck(userString) {
+    userString = userString.toLowerCase();
+
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,"");
+
+    let revString = [];
+    let returnObj = {};
+
+    for (let i = userString.length - 1; i >= 0 ; i--) {
+        revString += userString[i];
+    }
+
+    if (revString == userString) {
+        returnObj.msg = "Well done! You entered a palindrome!"
+    } else {
+        returnObj.msg = "Sorry, you did not enter a palindrome."
+    }
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+}
+
+function displayTacoCat(returnObj) {
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
